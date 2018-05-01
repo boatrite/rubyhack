0d92f8038e708344e7ad76f89ec71776da10e9e4
Gwen Boatrite <gwen.boatrite@gmail.com>
2018-04-30 22:26:07 -0500

    Small copypasta refactor fix to graph watch system

--- systems/graph_watch_system.rb
+++ systems/graph_watch_system.rb
@@ -6,28 +6,29 @@
 class GraphWatchSystem < Recs::System
 
   def process_one_game_tick(prev_em, em)
+    graph_component = em.get_simple Tag::GRAPH
+
     if prev_em.nil?
-      graph_component = em.get_simple Tag::GRAPH
-      current_node = graph_component.current_node
-      current_node.wall_coordinates.each do |i, j|
-        wall_entity = em.create_tagged_entity Tag::WALL
-        em.add_component wall_entity, Position.new(i, j, blocks: true)
-      end
+      initialize_node em, graph_component.current_node
     else
+      node_id = graph_component.current_node_id
+
       prev_graph_component = prev_em.get_simple Tag::GRAPH
       prev_node_id = prev_graph_component.current_node_id
 
-      graph_component = em.get_simple Tag::GRAPH
-      node_id = graph_component.current_node_id
-
-      if prev_node_id != node_id
+      if node_id != prev_node_id
         em.kill_entities em.get_entities_with_tag Tag::WALL
-
-        graph_component.current_node.wall_coordinates.each do |i, j|
-          wall_entity = em.create_tagged_entity Tag::WALL
-          em.add_component wall_entity, Position.new(i, j, blocks: true)
-        end
+        initialize_node em, graph_component.current_node
       end
+    end
+  end
+
+  private
+
+  def initialize_node(em, node)
+    node.wall_coordinates.each do |i, j|
+      wall_entity = em.create_tagged_entity Tag::WALL
+      em.add_component wall_entity, Position.new(i, j, blocks: true)
     end
   end
 end
