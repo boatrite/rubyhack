3fbf6da72bb31d6ff1b8e350073077ff1a36821c
Gwen Boatrite <gwen.boatrite@gmail.com>
2018-05-03 22:52:45 -0500

    Fix bug where player in different node with same i j could damage monsters

--- systems/player_input_system.rb
+++ systems/player_input_system.rb
@@ -41,106 +41,120 @@ class PlayerInputSystem < Recs::System
   end
 
   class DoDirectionHandler < InputHandler
+    def fire(em)
+      @next_player_position = Marshal.load Marshal.dump(
+        em.get_component_of_type_from_tag Tag::PLAYER, Position
+      )
+    end
+
     private
 
-    def process_movement(em, next_player_i, next_player_j)
+    def process_movement(em)
       # If the player is moving at an enemy, resolve attacking it. This removes
       # the entity if it is killed which will let the destination_empty check
       # pass for the spot the enemy had, so the player will move into that
       # spot.
-      monster_entities = em.get_entities_with_tag Tag::MONSTER # TODO I feel like I'm going to have an Attackable component
+      monster_entities = em.get_entities_with_tag Tag::MONSTER
       monster_entities.each do |entity|
-        process_attacking_monster em, entity, next_player_i, next_player_j
+        process_attacking_monster em, entity
       end
 
       # Figure out if there is something in the way of where the player is trying to move.
       world = em.get_component World
-      positions = em.get_components(Position)
+      destination_empty = em.get_components(Position).lazy
         .select { |position| position.node_id == world.current_node_id }
-      destination_empty = positions.reduce(true) { |is_empty, position|
-        if position.blocks?
-          is_empty && (next_player_i != position.i || next_player_j != position.j)
-        else
-          is_empty
-        end
-      }
+        .select { |position| @next_player_position.i == position.i && @next_player_position.j == position.j }
+        .select(&:blocks?)
+        .none?
 
       if destination_empty
         player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-        player_position.i = next_player_i
-        player_position.j = next_player_j
+        player_position.i = @next_player_position.i
+        player_position.j = @next_player_position.j
       end
     end
 
-    def process_attacking_monster(em, entity, next_player_i, next_player_j)
+    def process_attacking_monster(em, entity)
       # Matches where the monster is... (since it could have moved, but not necessarily).
       #
       # Since this system runs before MonsterAISystem, the monsters haven't moved yet.
       # That's why we get monster position from em instead of prev_em.
       monster_position = em.get_component_of_type entity, Position
+      return if monster_position.node_id != @next_player_position.node_id
+      return if @next_player_position.i != monster_position.i || @next_player_position.j != monster_position.j
 
       # Then deduct monster hp.
-      if next_player_i == monster_position.i && next_player_j == monster_position.j
-        monster_health = em.get_component_of_type entity, Health
-        monster_health.health -= 1
-        em.kill_entity entity if monster_health.health <= 0
-      end
+      monster_health = em.get_component_of_type entity, Health
+      monster_health.health -= 1
+      em.kill_entity entity if monster_health.health <= 0
     end
   end
 
   class DoLeft < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i, player_position.j - 1
+      super
+      @next_player_position.j -= 1
+      process_movement em
     end
   end
 
   class DoRight < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i, player_position.j + 1
+      super
+      @next_player_position.j += 1
+      process_movement em
     end
   end
 
   class DoUp < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i - 1, player_position.j
+      super
+      @next_player_position.i -= 1
+      process_movement em
     end
   end
 
   class DoDown < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i + 1, player_position.j
+      super
+      @next_player_position.i += 1
+      process_movement em
     end
   end
 
   class DoUpLeft < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i - 1, player_position.j - 1
+      super
+      @next_player_position.i -= 1
+      @next_player_position.j -= 1
+      process_movement em
     end
   end
 
   class DoUpRight < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i - 1, player_position.j + 1
+      super
+      @next_player_position.i -= 1
+      @next_player_position.j += 1
+      process_movement em
     end
   end
 
   class DoDownLeft < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i + 1, player_position.j - 1
+      super
+      @next_player_position.i += 1
+      @next_player_position.j -= 1
+      process_movement em
     end
   end
 
   class DoDownRight < DoDirectionHandler
     def fire(em)
-      player_position = em.get_component_of_type_from_tag Tag::PLAYER, Position
-      process_movement em, player_position.i + 1, player_position.j + 1
+      super
+      @next_player_position.i += 1
+      @next_player_position.j += 1
+      process_movement em
     end
   end
 
